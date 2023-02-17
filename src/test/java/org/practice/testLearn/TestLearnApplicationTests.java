95db8ea88a40bad47f79575bd9d4c24fcad5efe2
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2023-02-17 13:03:23 +0900

    Project Init

---
+++ src/test/java/org/practice/testLearn/TestLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package org.practice.testLearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

1c35449c893ae002774b83be5d94bbcb19b3c635
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2023-02-17 13:58:25 +0900

    feat : ProductServiceTest

--- src/test/java/org/practice/testLearn/productOrderService/product/ProductServiceTest.java
+++ src/test/java/org/practice/testLearn/productOrderService/product/ProductServiceTest.java
@@ -1,10 +1,24 @@
 package org.practice.testLearn.productOrderService.product;
 
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.util.Assert;
 
 class ProductServiceTest {
+
+    private ProductService productService;
+    private ProductPort productPort;
+    private ProductRepository productRepository;
+
+    @BeforeEach
+    void setUp() {
+        productRepository = new ProductRepository();
+        productPort = new ProductAdapter(productRepository);
+        productService = new ProductService(productPort);
+    }
 
     @DisplayName("상품 등록 서비스 테스트")
     @Test
@@ -14,15 +28,31 @@ class ProductServiceTest {
         DiscountPolicy discountPolicy = DiscountPolicy.NONE;
         final AddProductRequest request = new AddProductRequest(name, money, discountPolicy);
 
+        productService.addProduct(request);
     }
 
-    private static class AddProductRequest {
+    private class ProductService {
 
+        private final ProductPort productPort;
+
+        private ProductService(ProductPort productPort) {
+            this.productPort = productPort;
+        }
+
+        public void addProduct(AddProductRequest request) {
+            final Product product = new Product(request.name, request.price, request.discountPolicy);
+            productPort.save(product);
+        }
+    }
+
+    private class Product {
+
+        private  Long id;
         private final String name;
         private final int price;
         private final DiscountPolicy discountPolicy;
 
-        public AddProductRequest(String name, int price, DiscountPolicy discountPolicy) {
+        public Product(String name, int price, DiscountPolicy discountPolicy) {
             Assert.hasText(name, "상품명은 필수입니다.");
             Assert.isTrue(price > 0, "상품 가격은 0보다 커야합니다.");
             Assert.notNull(discountPolicy, "할인 정책은 필수입니다.");
@@ -30,10 +60,57 @@ class ProductServiceTest {
             this.price = price;
             this.discountPolicy = discountPolicy;
         }
+
+        public void assignId(Long id) {
+            this.id = id;
+        }
+
+        public Long getId() {
+            return id;
+        }
+    }
+    private record AddProductRequest(String name, int price, DiscountPolicy discountPolicy) {
+
+        private AddProductRequest {
+            Assert.hasText(name, "상품명은 필수입니다.");
+            Assert.isTrue(price > 0, "상품 가격은 0보다 커야합니다.");
+            Assert.notNull(discountPolicy, "할인 정책은 필수입니다.");
+        }
+
     }
 
     private enum DiscountPolicy {
         NONE
 
+    }
+
+    private interface ProductPort {
+
+        void save(Product product);
+    }
+
+    private class ProductAdapter implements ProductPort {
+
+        private final ProductRepository productRepository;
+
+        private ProductAdapter(ProductRepository productRepository) {
+            this.productRepository = productRepository;
+        }
+
+        @Override
+        public void save(Product product) {
+            productRepository.save(product);
+        }
+    }
+
+    private class ProductRepository {
+
+        private Map<Long, Product> persistence = new HashMap<>();
+        private Long sequence = 0L;
+
+        public void save(Product product) {
+            product.assignId(++sequence);
+            persistence.put(product.getId(), product);
+        }
     }
 }
